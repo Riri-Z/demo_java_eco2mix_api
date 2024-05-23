93c84845edf8fcdba0dbff24549fbd2fcfef6197
Riri-Z <riri.zamor@gmail.com>
2024-05-23 14:42:12 +0200

    Init

---
+++ src/main/java/com/eco2mix/Eco2mixApplication.java
@@ -0,0 +1,13 @@
+package com.eco2mix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eco2mixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eco2mixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eco2mix/Eco2mixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco2mix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eco2mixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
