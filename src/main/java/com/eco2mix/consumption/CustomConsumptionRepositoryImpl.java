11a6a76cf3f70b1f6efb9dd1cf406f41997e8fe0
Riri-Z <riri.zamor@gmail.com>
2024-06-04 15:09:41 +0200

    Add constant

--- src/main/java/com/eco2mix/consumption/CustomConsumptionRepositoryImpl.java
+++ src/main/java/com/eco2mix/consumption/CustomConsumptionRepositoryImpl.java
@@ -1,7 +1,5 @@
 package com.eco2mix.consumption;
 
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
-
 import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -13,27 +11,35 @@ import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 
 class CustomConsumptionRepositoryImpl implements CustomConsumptionRepository {
+  private static final String CONSOMMATION_BRUT_ELECTRICITE = "consommation_brute_electricite_rte";
+  private static final String CONSOMMATION_BRUT_GAZ = "consommation_brute_gaz_totale";
+  private static final String INSEE_FIELD = "code_insee_region";
+  private static final String DATE_FIELD = "date";
+  private static final String REGION_FIELD = "region";
+
   private final MongoTemplate mongoTemplate;
 
   public CustomConsumptionRepositoryImpl(MongoTemplate mongoTemplate) {
     this.mongoTemplate = mongoTemplate;
-  };
+  }
 
   @Override
   public List<Consumption> getConsumptionByDate(String date) {
 
-    MatchOperation matchStage = match(new Criteria("date").is(date));
+    // Filter by date
+    MatchOperation matchStage = Aggregation.match(new Criteria(DATE_FIELD).is(date));
 
-    GroupOperation groupStage = group("code_insee_region").sum("consommation_brute_gaz_totale")
-        .as("consommation_brute_gaz_totale")
-        .sum("consommation_brute_electricite_rte").as("consommation_brute_electricite_rte").first("code_insee_region")
-        .as("code_insee_region");
-    ;
+    // Group data by region
+    GroupOperation groupStage = Aggregation.group(INSEE_FIELD, REGION_FIELD).sum(CONSOMMATION_BRUT_GAZ)
+        .as(CONSOMMATION_BRUT_GAZ)
+        .sum(CONSOMMATION_BRUT_ELECTRICITE).as(CONSOMMATION_BRUT_ELECTRICITE).first(INSEE_FIELD)
+        .as(INSEE_FIELD);
 
-    ProjectionOperation projectStage = project("code_insee_region", "consommation_brute_gaz_totale",
-        "consommation_brute_electricite_rte", "date", "region").andExclude("_id");
+    // Keep only needed field
+    ProjectionOperation projectStage = Aggregation.project(INSEE_FIELD, CONSOMMATION_BRUT_GAZ,
+        CONSOMMATION_BRUT_ELECTRICITE, DATE_FIELD, REGION_FIELD).andExclude("_id");
 
-    Aggregation aggregation = newAggregation(matchStage, groupStage, projectStage);
+    Aggregation aggregation = Aggregation.newAggregation(matchStage, groupStage, projectStage);
     AggregationResults<Consumption> results = mongoTemplate.aggregate(aggregation, "consumption", Consumption.class);
 
     return results.getMappedResults();
